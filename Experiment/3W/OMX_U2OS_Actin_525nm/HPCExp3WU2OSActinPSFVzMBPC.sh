13a269c8f7653b6cc2a265971b30ffa6b0be4e29
Cong <cvan@memphis.edu>
2020-02-26 16:16:31 -0600

    Fix a HPC script to be 2 days.

--- Experiment/3W/OMX_U2OS_Actin_525nm/HPCExp3WU2OSActinPSFVzMBPC.sh
+++ Experiment/3W/OMX_U2OS_Actin_525nm/HPCExp3WU2OSActinPSFVzMBPC.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem-per-cpu=5000M
-#SBATCH --time=1-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=cvan@memphis.edu
 
