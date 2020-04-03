9e3f55fb71ceb9fe7c8735ca5a803fbfcdae87a8
Cong Van <van.congtuanson@gmail.com>
2020-04-03 09:37:51 -0500

    Add MBPC with DR 7 out of 15.

---
+++ Simulation/3W/OSSRMultiOb/HPCSim3WDR7of15OSSRMultiOb256SNR15dB.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition computeq
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=3000M
+#SBATCH --time=0-00:30:00
+
+# just in case purge the old modules and load Matlab module
+module purge
+module load matlab
+
+# run matlab program via the run_matlab script
+logFile=Sim3WDR7of15OSSRMultiOb256SNR15dB.log
+mFile=Sim3WDR7of15OSSRMultiOb256SNR15dB.m
+/public/apps/matlab/R2018a/bin/matlab -nodisplay -nosplash -nodesktop -logfile $logFile -r "run $mFile;quit;"
---
+++ Simulation/3W/OSSRMultiOb/Sim3WDR7of15OSSRMultiOb256SNR15dB.m
@@ -0,0 +1,24 @@
+run("Sim3WOSSRMultiOb256Setup.m");
+
+%% load the noiseless data
+matFile = CIRLDataPath + "/Simulation/3W/Sim3WOSSRMultiOb256SNR15dB.mat";
+load(matFile, 'g');
+
+%% set the data reduction phases and index
+thePhi    = [  0 0;   0 72*2; 0 144*2; 0 216*2; 0 288*2; ...
+                     60 72*2; ...
+                    120 72*2];
+thePhiInd = [1 1; 1 2; 1 3; 1 4; 1 5; ...
+                  2 2; ...
+                  3 2];
+Npt       = length(thePhi);
+
+%% down-sample with data reduction
+for ind = 1:Npt
+    gDR(:,:,:,ind) = g(:, :, :, thePhiInd(ind,1), thePhiInd(ind,2));
+end
+
+%% add the noise
+matFile = CIRLDataPath + "/Simulation/3W/Sim3WDR7of15OSSRMultiOb256SNR15dB.mat";
+g = gDR;
+save(matFile, '-v7.3', 'g');
