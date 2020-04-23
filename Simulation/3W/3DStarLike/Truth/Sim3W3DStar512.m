b10eed191a778f1a9913af0e1c097b7a357787f9
Cong Van <van.congtuanson@gmail.com>
2020-04-23 09:03:02 -0500

    Fix a bug in star-like generation. Add 15dB noises to star-like data.

--- Simulation/3W/3DStarLike/Truth/Sim3W3DStar512.m
+++ Simulation/3W/3DStarLike/Truth/Sim3W3DStar512.m
@@ -2,7 +2,7 @@ run("Sim3W3DStar512Setup.m");
 matFile = CIRLDataPath + "/Simulation/3W/Sim3W3DStar512.mat";
 
 %% run forward model and save the results into CIRLDataPath
-ob = OSSRMultiObject(X, Z, dXY, dZ);
+ob = StarLikeSample(3,512,8,20,3,0.7);
 g  = ForwardModel(ob, h, im, jm);
 save(matFile, '-v7.3', 'g', 'ob');
 
