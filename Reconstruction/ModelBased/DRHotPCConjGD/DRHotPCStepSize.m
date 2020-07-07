e4ee959cc490533dd760db59935edf3dd61a06f1
Cong Van <van.congtuanson@gmail.com>
2020-07-07 09:38:31 -0500

    Add DR with WF for MBPC method.

--- Reconstruction/ModelBased/DRHotPCConjGD/DRHotPCStepSize.m
+++ Reconstruction/ModelBased/DRHotPCConjGD/DRHotPCStepSize.m
@@ -1,4 +1,7 @@
-function curStepSize = DRHotPCStepSize(ForwardModelFct, UpFct, g0, d, xi, h, imFunc, jmFunc, Nm, xx, yy, zz, dXY, dZ, omegaXY, omegaZ, thePhiOff, phizDeg)
+function curStepSize = DRHotPCStepSize(ForwardModelFct, UpFct, g0, d, xi, h, imFunc, jmFunc, Nm, xx, yy, zz, dXY, dZ, omegaXY, omegaZ, thePhiOff, phizDeg, isDRWF)
+if (nargin < 19)
+    isDRWF = 0;
+end
 [~, ~, ~, Npt] = size(g0);
 [X, Y, Z] = size(xi);
 AA = 0;
@@ -9,12 +12,19 @@ for lk = 1:Npt
         A1 = zeros(Y, X, Z);
         A2 = zeros(Y, X, Z);
         A3 = zeros(Y, X, Z);
-        for m = 1:Nm
-            jmTemp = jmFunc(xx, yy, dXY, omegaXY(1), thePhiOff(lk,1), thePhiOff(lk,2), thePhiOff(lk,3), m);
-            FThim  = FT(h.*imFunc(zz, dZ, omegaZ(1), phizDeg, m));
-            A1     = A1 + real(IFT( FT(xi.*xi.*jmTemp) .* FThim ));
-            A2     = A2 - real(IFT( FT(d .*xi.*jmTemp) .* FThim ));
-            A3     = A3 - real(IFT( FT(d .*d .*jmTemp) .* FThim ));
+        if isDRWF && lk == Npt
+            FThim  = FT(h.*imFunc(zz, dZ, omegaZ(1), phizDeg, 1));
+            A1     = A1 + real(IFT( FT(xi.*xi) .* FThim ));
+            A2     = A2 - real(IFT( FT(d .*xi) .* FThim ));
+            A3     = A3 - real(IFT( FT(d .*d ) .* FThim ));
+        else
+            for m = 1:Nm
+                jmTemp = jmFunc(xx, yy, dXY, omegaXY(1), thePhiOff(lk,1), thePhiOff(lk,2), thePhiOff(lk,3), m);
+                FThim  = FT(h.*imFunc(zz, dZ, omegaZ(1), phizDeg, m));
+                A1     = A1 + real(IFT( FT(xi.*xi.*jmTemp) .* FThim ));
+                A2     = A2 - real(IFT( FT(d .*xi.*jmTemp) .* FThim ));
+                A3     = A3 - real(IFT( FT(d .*d .*jmTemp) .* FThim ));
+            end
         end
         A1 = UpFct(-A1, g0(:,:,:,lk));
         AA = AA +   dot(A3(:), A3(:));
@@ -32,9 +42,13 @@ cost_alpha = zeros(length(loc_alpha),1);
 for loc = 1:length(loc_alpha)
     if (isreal(loc_alpha(loc)))
         for lk = 1:Npt
+            isWF = 0;	
+            if isDRWF && (lk == Npt)	
+                isWF = 1;	
+            end
                 diff = ForwardModelFct((xi + loc_alpha(loc)*d).^2, h, imFunc, jmFunc, Nm, xx,     ...
                                        yy, zz, dXY, dZ, omegaXY(1), omegaZ(1), thePhiOff(lk,1), ...
-                                       thePhiOff(lk,2), thePhiOff(lk,3), phizDeg);
+                                       thePhiOff(lk,2), thePhiOff(lk,3), phizDeg, isWF);
                 diff = UpFct(-diff, g0(:,:,:,lk));
                 cost_alpha(loc) = cost_alpha(loc) + dot(diff(:), diff(:));
         end
---
+++ Reconstruction/ModelBased/DRWFHotPCGradientDescent.m
@@ -0,0 +1,104 @@
+function [reconOb, retFig, retVars] = DRWFHotPCGradientDescent(...
+            ForwardFct, GradientFct, StepSizeFct, UpFct, imFct, jmFct,...
+            g, h, dXY, dZ, Nm, omegaXY, omegaZ, thePhiOff, phiz, ...
+            numIt, penalizationParam, initGuess, selectDir, picInterval, gTransform)
+fprintf('Call to %s\n',  mfilename);
+tNow    = datestr(datetime(),'yyyymmddHHMMSS')
+curFig  = tNow + ".jpg";
+retFig  = [];
+retVars = {};
+DEBUG_DISPLAY = 1;
+
+if(nargin < 16)
+    numIt = 10;
+end
+
+if(nargin < 17 || isequal(penalizationParam , -1))
+    penalizationParam = 1e-8;
+end
+
+if(nargin < 19 || isequal(selectDir , -1) )
+    selectDir = 2;
+end
+
+if(nargin < 20)
+    picInterval = -1;
+end
+
+% normalize the data and zero-padding
+[Y, X, Z, Npt] = size(g);
+X   = X*2;
+Y   = Y*2;
+Z   = Z + floor(Z/2)*2;
+[yy, xx, zz] = meshgrid(0 : 1 : X-1, 0 : 1 : Y-1, 0 : 1 : Z-1);
+
+if(nargin < 21)
+    initInd = Npt;
+end
+
+xi        = zeros(X,Y,Z);
+memGrad   = zeros(X,Y,Z);
+desctDir  = zeros(X,Y,Z);
+xi        = UpFct(xi, g(:,:,:,end)); % last one is the widefield
+xi        = sqrt(xi);
+
+% end
+tic;
+for it = 1:numIt
+    grad = zeros(X,Y,Z);
+    crit = 0;
+    diff = zeros(X, Y, Z, Npt);
+    for lk = 1:Npt  
+        isWF = 0;	
+        if (lk == Npt)	
+            isWF = 1;	
+        end
+        diff(:,:,:,lk) = ForwardFct(xi.^2, h, imFct, jmFct, Nm, xx,     ...
+                                    yy, zz, dXY, dZ, omegaXY(1), omegaZ(1), thePhiOff(lk,1), ...
+                                    thePhiOff(lk,2), thePhiOff(lk,3), phiz, isWF);
+        diff(:,:,:,lk) = UpFct(-diff(:,:,:,lk), g(:,:,:,lk));
+        grad = GradientFct( grad, diff(:,:,:,lk), h, imFct, jmFct, Nm, xx,     ...
+            yy, zz, dXY, dZ, omegaXY(1), omegaZ(1), thePhiOff(lk,1), ...
+            thePhiOff(lk,2), thePhiOff(lk,3), phiz, isWF);
+        %crit = crit + gather(dot(diff(:), diff(:)));
+    end
+    crit = crit + dot(diff(:), diff(:));
+    grad = -real(4.*xi.*grad);
+    if (it == 1)
+        memGrad  =  grad;
+        desctDir = -grad;
+    end
+    
+    desctDir = grad + dot(grad(:),(grad(:) - memGrad(:)))/ dot(memGrad(:),memGrad(:))*desctDir;
+    
+    alpha = StepSizeFct(ForwardFct, UpFct, g, desctDir, xi, h, imFct, jmFct, Nm, xx,     ...
+                        yy, zz, dXY, dZ, omegaXY, omegaZ, thePhiOff, phiz, 1);
+    fprintf('It : %d \n',it);
+    fprintf('Alpha : %03.2e    crit : %d\n',alpha, crit);
+    
+    xi      = xi + alpha.*desctDir;
+    memGrad = grad;
+    
+    if (picInterval ~= -1 && mod(it, picInterval) == 0)
+        reconOb = xi.^2;
+        retFig(end+1)  = figure('Position', get(0, 'Screensize'));
+        imagesc(reconOb(:,:,1+round(Z/2)));
+        xlabel('x'); ylabel('y'); colorbar; axis square; colormap jet;
+        title("it = " + num2str(it));
+        retVars{end+1} = reconOb;
+        saveas(retFig(end), it + "_" + curFig);
+        save(tNow + ".mat", 'retVars', '-v7.3')
+    end
+    
+    if (mod(it, 10) == 0)
+        reconOb = xi.^2;
+        fig    = figure('Position', get(0, 'Screensize'));
+        imagesc(reconOb(:,:,1+round(Z/2)));
+        xlabel('x'); ylabel('y'); colorbar; axis square; colormap jet;
+        title("it = " + num2str(it));
+        saveas(fig, curFig);
+    end
+end
+reconOb = xi.^2;
+toc;
+end
